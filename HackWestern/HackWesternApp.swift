152a03b2e1d65a732ed05b0abd30eff6ed398c84
krpchandok <kirpachandok@gmail.com>
2024-11-30 04:56:27 -0500

    Initial Commit

---
+++ HackWestern/HackWesternApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackWesternApp.swift
+//  HackWestern
+//
+//  Created by Kirpa Chandok on 2024-11-30.
+//
+
+import SwiftUI
+
+@main
+struct HackWesternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackWesternTests/HackWesternTests.swift
@@ -0,0 +1,17 @@
+//
+//  HackWesternTests.swift
+//  HackWesternTests
+//
+//  Created by Kirpa Chandok on 2024-11-30.
+//
+
+import Testing
+@testable import HackWestern
+
+struct HackWesternTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
